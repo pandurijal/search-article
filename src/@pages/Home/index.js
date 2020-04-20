e2a9b91139104c61818fab0fffbb44858128d661
pandurijal <pandurijal96@gmail.com>
2020-04-20 16:02:47 +0700

    initialize folder structure

---
+++ src/@pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Search, Table } from './../../@components';
+
+const Home = () => {
+  return (
+    <>
+      <Search />
+      <Table />
+    </>
+  );
+};
+
+export default Home;
